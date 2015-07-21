85041de6115a289898c312eebde6d787d0aebafe
kaiye <catgecn@gmail.com>
2015-07-21 22:13:44 +0800

    based on https://github.com/mz-components/components/blob/master/modules/jquery-ui.js

---
+++ effect-transfer.js
@@ -0,0 +1,25 @@
+/*!
+ * jQuery UI Effects Transfer @VERSION
+ * http://jqueryui.com
+ *
+ * Copyright jQuery Foundation and other contributors
+ * Released under the MIT license.
+ * http://jquery.org/license
+ */
+//>>label: Transfer Effect
+//>>group: Effects
+//>>description: Displays a transfer effect from one element to another.
+//>>docs: http://api.jqueryui.com/transfer-effect/
+//>>demos: http://jqueryui.com/effect/
+
+(function (factory) {
+    // AMD. Register as an anonymous module.
+    
+    module.exports = factory(require('jquery'), require('./effect')) || module.exports;;
+}(function ($) {
+    if ($.uiBackCompat !== false) {
+        return $.effects.define('transfer', function (options, done) {
+            $(this).transfer(options, done);
+        });
+    }
+}));
---
+++ form-reset-mixin.js
@@ -0,0 +1,44 @@
+
+(function (factory) {
+    // AMD. Register as an anonymous module.
+    
+    module.exports = factory(require('jquery'), require('ui/core')) || module.exports;;
+}(function ($) {
+    return $.ui.formResetMixin = {
+        _formResetHandler: function () {
+            var form = $(this);
+            // Wait for the form reset to actually happen before refreshing
+            setTimeout(function () {
+                var instances = form.data('ui-form-reset-instances');
+                $.each(instances, function () {
+                    this.refresh();
+                });
+            });
+        },
+        _bindFormResetHandler: function () {
+            this.form = this.element.form();
+            if (!this.form.length) {
+                return;
+            }
+            var instances = this.form.data('ui-form-reset-instances') || [];
+            if (!instances.length) {
+                // We don't use _on() here because we use a single event handler per form
+                this.form.on('reset.ui-form-reset', this._formResetHandler);
+            }
+            instances.push(this);
+            this.form.data('ui-form-reset-instances', instances);
+        },
+        _unbindFormResetHandler: function () {
+            if (!this.form.length) {
+                return;
+            }
+            var instances = this.form.data('ui-form-reset-instances');
+            instances.splice($.inArray(this, instances), 1);
+            if (instances.length) {
+                this.form.data('ui-form-reset-instances', instances);
+            } else {
+                this.form.removeData('ui-form-reset-instances').off('reset.ui-form-reset');
+            }
+        }
+    };
+}));
